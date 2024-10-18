baa22da254a4215db4a245daa4fa555ab3386640
JeffreyZhu <1624410543@qq.com>
2024-10-18 23:01:59 +0800

    添加jwtService

--- src/main/java/com/example/springboot3security/repositories/UserRepository.java
+++ src/main/java/com/example/springboot3security/repositories/UserRepository.java
@@ -1,12 +1,14 @@
 package com.example.springboot3security.repositories;
 
 import com.tericcabrel.authapi.entities.User;
+import jakarta.annotation.Resource;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
+@Resource
 public interface UserRepository extends CrudRepository<User, Integer> {
     Optional<User> findByEmail(String email);
 }
